b441d3e4b587b290ba64b219bb37b263bd6ba5aa
Bee-Kay-Pee <106575847+Bee-Kay-Pee@users.noreply.github.com>
2023-10-09 15:57:25 +1030

    Create items.lua

---
+++ items.lua
@@ -0,0 +1,13 @@
+
+
+-- katanas 
+
+
+    ['weapon_katana_blue']			    = {['name'] = 'weapon_katana_blue',				    ['label'] = 'Blue Katana',				['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_blue.png',		        ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a blue handle'},
+    ['weapon_katana_dark_blue']			= {['name'] = 'weapon_katana_dark_blue',		    ['label'] = 'Dark Blue Katana',			['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_dark_blue.png',		    ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a dark blue handle'},
+    ['weapon_katana_green']			    = {['name'] = 'weapon_katana_green',				['label'] = 'Green Katana',				['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_green.png',		        ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a green handle'},
+    ['weapon_katana_lilac']			    = {['name'] = 'weapon_katana_lilac',				['label'] = 'Lilac Katana',				['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_lilac.png',		        ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a lilac handle'},
+    ['weapon_katana_pink']			    = {['name'] = 'weapon_katana_pink',				    ['label'] = 'Pink Katana',				['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_pink.png',		        ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a pink handle'},
+    ['weapon_katana_purple']			= {['name'] = 'weapon_katana_purple',				['label'] = 'Purple Katana',			['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_purple.png',		        ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a purple handle'},
+    ['weapon_katana_red']			    = {['name'] = 'weapon_katana_red',				    ['label'] = 'Red Katana',				['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_red.png',		        ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a red handle'},
+    ['weapon_katana_yellow']			= {['name'] = 'weapon_katana_yellow',				['label'] = 'Yellow Katana',			['weight'] = 1000,		   ['type'] = 'weapon',	    ['ammotype'] = nil,						   ['image'] = 'katana_yellow.png',		        ['unique'] = true,		   ['useable'] = true,		['description'] = 'A katana with a yellow handle'},
